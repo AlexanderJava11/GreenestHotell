fc4a79a61b0ca882a0f2959b46e3ba1b8e0b92a8
Alexander <alexander.zeljic@yh.nackademin.se>
2025-10-08 16:42:08 +0200

    My interface named Vätskebehov with comments

--- Vätskebehov.java
+++ Vätskebehov.java
@@ -1,9 +1,12 @@
 package Inlämningsuppgift1;
 
+/*
+Interface som definierar vad alla växter.
+Används med polymorfism
+ */
+
 public interface Vätskebehov {
     VätskeTyp getVätskeTyp();
-
     double getDagligVätskaLitar();
-
     String getNamn();
 }
